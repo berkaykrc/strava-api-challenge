3e8858209fa43f98f14d0af200495561816efe3c
Berkay Kırcı <berkay.kirci@gmail.com>
2018-07-26 00:28:24 +0300

    code fixing turned into pythonic code

--- strava/views.py
+++ strava/views.py
@@ -1,17 +1,15 @@
+import collections
+import itertools
+
+import requests as r
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-import requests as r
-# import ipdb
-
-
-segments = []
-duplicate_bikers = []
-all_names = []
 
 
 @api_view()
 def source(request):
-
+    segments = []
+    all_names = []
     segments_explore = 'https://www.strava.com/api/v3/segments/explore?'
     bounds = '&bounds=40.771394267343176, 26.488085546875027, 41.598195829044954, 31.607714453125027'  # istanbul
     access_token = '&access_token=37d4400d9157c0d7b0237e5a5568d9af41272462'  # access
@@ -33,22 +31,14 @@ def source(request):
                 leaderboard_names.append(str(names["athlete_name"]))
             all_names.append(leaderboard_names)
         # ipdb.set_trace()
+        return Response([
+            item
+            for item, count in collections.Counter(
+                itertools.chain.from_iterable(all_names)
+            ).items()
+            if count > 1
+        ])
 
-        kontrol = False
-        for j in range(len(segments)):
-            for i in range(len(segments)):
-                for k in range(len(segments)):
-                    for m in range(len(segments)):
-                        if j != k and m != i:
-                            if all_names[j][i] == all_names[k][m]:
-                                for a in range(len(duplicate_bikers)):
-                                    if all_names[j][i] == duplicate_bikers[a]:
-                                        kontrol = True
-                                if kontrol is False:
-                                    duplicate_bikers.append(all_names[j][i])
-                                else:
-                                    kontrol = False
-        return Response(duplicate_bikers)
-
+        '''[name for name, count in Counter(chain(all_names)).items() if count > 1]'''
     except Exception as x:
         print("Your Exception is :", x)
